1dbdaa3bf2911e2ec9b8b1680d08d574d1f26d01
tima <tima.stella88@gmail.com>
2026-05-09 20:36:21 +0300

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -28,31 +28,22 @@
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
-
-        <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
             <img class="animation__shake" src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
         </div>
 
-        <!-- Navbar -->
         @include('admin.layouts.part.header')
-        <!-- /.navbar -->
 
-        <!-- Main Sidebar Container -->
         @include('admin.layouts.sidebar')
-        <!-- Content Wrapper. Contains page content -->
+
         @include('admin.layouts.widget')
-        <!-- /.content-wrapper -->
+
         @include('admin.layouts.part.footer')
 
-        <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
             <!-- Control sidebar content goes here -->
         </aside>
-        <!-- /.control-sidebar -->
     </div>
-<!-- ./wrapper -->
-
     <!-- jQuery -->
     <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
     <!-- jQuery UI 1.11.4 -->
