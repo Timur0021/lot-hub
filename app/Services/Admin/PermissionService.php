05e2a57cac85c7b4ce6f459c3915a3b277c91cf9
tima <tima.stella88@gmail.com>
2026-06-22 22:41:39 +0300

    add permissions

--- app/Services/Admin/PermissionService.php
+++ app/Services/Admin/PermissionService.php
@@ -10,9 +10,17 @@ class PermissionService
 
         $actions = [
             'view' => 'View',
+            'view_all' => 'View All',
             'create' => 'Create',
             'update' => 'Update',
             'delete' => 'Delete',
+            'delete_any' => 'Delete Any',
+            'force_delete' => 'Force Delete',
+            'force_delete_any' => 'Force Delete Any',
+            'restore' => 'Restore',
+            'restore_any' => 'Restore Any',
+            'replicate' => 'Replicate',
+            'reorder' => 'Reorder',
         ];
 
         $actionLabel = $actions[$action] ?? ucfirst($action);
