1abb7288338817b85200dcd053e4e7b9845ae97a
tima <tima.stella88@gmail.com>
2026-08-21 12:54:42 +0300

    updare route

--- routes/admin/sections/chat.php
+++ routes/admin/sections/chat.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Admin\Chat\MessageController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('chat')->group(function () {
-    Route::get('/messages/{userId}', [MessageController::class, 'index'])->name('chat.messages.index');
-    Route::post('/messages', [MessageController::class, 'store'])->name('chat.messages.store');
-});
+Route::prefix('chat')
+    ->name('chat.')
+    ->group(function () {
+        Route::get('/messages/{userId}', [MessageController::class, 'index'])->name('messages.index');
+        Route::post('/messages', [MessageController::class, 'store'])->name('messages.store');
+    });
