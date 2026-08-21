ac658d650163f4655d85c6f4278c778d2b83a5cf
tima <tima.stella88@gmail.com>
2026-08-21 14:43:56 +0300

    updare front

--- resources/views/front/layouts/index.blade.php
+++ resources/views/front/layouts/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Title For This Document -->
-    <title> Karte - Multipurpose E-Commerce Html Template</title>
+    <title>@yield('title')</title>
     <!-- Favicon For This Document -->
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon-32x32.png') }}" type="image/x-icon">
     <!-- Bootstrap 5 Css -->
